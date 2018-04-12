75367b98e4b95e7209f03b7993502c2234cb745d
mounirzer <38168068+mounirzer@users.noreply.github.com>
2018-04-12 19:31:06 +0100

    mlsmlk
    Co-Authored-By: vidivici7 <vidivici7@users.noreply.github.com>

--- admin/main/aside.php
+++ admin/main/aside.php
@@ -1,15 +1,15 @@
    <ul id="sidebarnav">
                         <li> <a class="waves-effect waves-dark" href="index.php" aria-expanded="false"><i class="fa fa-user-circle-o"></i><span class="hide-menu">Accueil</span></a>
                         </li>
-                        <li> <a class="waves-effect waves-dark" href="AddArt.php" aria-expanded="false"><i class="fa fa-bookmark-o"></i><span class="hide-menu">Ajouter Article</span></a>
+                        <li> <a class="waves-effect waves-dark" href="addart.php" aria-expanded="false"><i class="fa fa-bookmark-o"></i><span class="hide-menu">Ajouter Article</span></a>
                         </li>
-                        <li> <a class="waves-effect waves-dark" href="Articles.php" aria-expanded="false"><i class="fa fa-table"></i><span class="hide-menu">Suprimer / Modifier</span></a>
+                        <li> <a class="waves-effect waves-dark" href="articles.php" aria-expanded="false"><i class="fa fa-table"></i><span class="hide-menu">Suprimer / Modifier</span></a>
                         </li>
-                        <li> <a class="waves-effect waves-dark" href="AddDoc.php" aria-expanded="false"><i class="fa fa-question-circle"></i><span class="hide-menu">Ajouter Document</span></a>
+                        <li> <a class="waves-effect waves-dark" href="adddoc.php" aria-expanded="false"><i class="fa fa-question-circle"></i><span class="hide-menu">Ajouter Document</span></a>
                         </li>
                         <li> <a class="waves-effect waves-dark" href="documents.php" aria-expanded="false"><i class="fa fa-table"></i><span class="hide-menu">Suprimer / Mdifier</span></a>
                         </li>
-                        <li> <a class="waves-effect waves-dark" href="AddAct.php" aria-expanded="false"><i class="fa fa-globe"></i><span class="hide-menu">Ajouter Actualité</span></a>
+                        <li> <a class="waves-effect waves-dark" href="addact.php" aria-expanded="false"><i class="fa fa-globe"></i><span class="hide-menu">Ajouter Actualité</span></a>
                         </li>
                         <li> <a class="waves-effect waves-dark" href="actualites.php" aria-expanded="false"><i class="fa fa-table"></i><span class="hide-menu">Suprimer / Modifier</span></a>
                         </li>
