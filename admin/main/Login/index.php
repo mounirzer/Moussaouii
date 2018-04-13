aaf8b41ea77e57c3fd92a443fc3d5cd5bd9c8b92
mounirzer <38168068+mounirzer@users.noreply.github.com>
2018-04-13 23:47:08 +0100

    d

--- admin/main/Login/index.php
+++ admin/main/Login/index.php
@@ -32,14 +32,12 @@
 		<div class="container-login100">
 			<div class="wrap-login100 p-t-85 p-b-20">
 				<form class="login100-form validate-form" method="post" action="login.php">
-					<span class="login100-form-title p-b-70">
-						Welcome
-					</span>
+					
 					<span class="login100-form-avatar">
 						<img src="images/Capture.jpg" alt="AVATAR">
 					</span>
 					<div class="wrap-input100 validate-input m-t-85 m-b-35" data-validate = "Enter username">
-						<input class="input100" type="text" value="hhhhh"  name="username">
+						<input class="input100" type="text"   name="username">
 						<span class="focus-input100" data-placeholder="Username"></span>
 					</div>
 					<div class="wrap-input100 validate-input m-b-50" data-validate="Enter password">
