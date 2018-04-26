ae35ce1c1ea418298858a6c33be171c9ec606ff6
mounirzer <38168068+mounirzer@users.noreply.github.com>
2018-04-26 17:59:33 +0100

    Revert "Revert "uhiuh""
    This reverts commit 1ccac7d33addc5d8705df1342eeafeb90e58fb28.

--- header.php
+++ header.php
@@ -8,10 +8,10 @@
       <nav id="nav-menu-container">
         <ul class="nav-menu">
           <li class="menu-active"><a href="#intro">Accueil</a></li>
-          <li><a href="index.php#about">À propos</a></li>
-          <li><a href="index.php#action">Articles</a></li>
-          <li><a href="index.php#call-to-action">Documents</a></li>
-          <li><a href="index.php#contact">Contact</a></li>
+          <li><a href="#about">À propos</a></li>
+          <li><a href="#action">Articles</a></li>
+          <li><a href="#call-to-action">Documents</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </nav><!-- #nav-menu-container -->
     </div>
@@ -21,21 +21,21 @@
       <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
           <ol class="carousel-indicators"></ol>
           <div class="carousel-inner" role="listbox">
-    <?php $con = mysqli_connect("localhost", "root", "", "siteweb");
+     <?php $con = mysqli_connect("localhost", "root", "", "siteweb");
      $sql = "SELECT * FROM actuallite";
-    $result = mysqli_query($con, $sql);$i=0;
-     while($row = mysqli_fetch_assoc($result)){ ?>
+     $result = mysqli_query($con, $sql);$i=1;
+     while($row = mysqli_fetch_assoc($result)){?>
           <div class="carousel-item <?php if ($i==1) {echo "active";}?>">
-            <div class="carousel-background"><img src="<?php echo $row["LienImage"]; ?>" alt=""></div>
+            <div class="carousel-background"><img src="<?php echo substr($row["LienImage"],9) ; ?>" alt=""></div>
             <div class="carousel-container">
               <div class="carousel-content">
-                <h2><?php echo $row["titre"]; ?></h2>
-                    <p><?php echo $row["contenu"]; ?></p>
+                <h2><?php echo $row["titreactua"]; ?></h2>
+                    <p><?php echo $row["contenuactua"]; ?></p>
                 <!--a href="#featured-services" class="btn-get-started scrollto">Get Started</a-->
               </div>
             </div>
          </div>
-            <?php } ?>
+            <?php $i=$i+1; } ?>
       </div>
         <a class="carousel-control-prev" href="#introCarousel" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon ion-chevron-left" aria-hidden="true"></span>
@@ -47,5 +47,5 @@
         </a>
       </div>
     </div>
-  </section>
+  </section><!-- #intro -->
   <main id="main">
